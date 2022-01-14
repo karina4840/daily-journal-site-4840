d8ff5d5abadd3f7eceae86ce4708311b3315808a
karina ivanova <86211129+karina4840@users.noreply.github.com>
2022-01-14 15:52:31 +0800

    major content changes
    added content describing the app and a contact form

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ const {
   forEach,
   lowerCase
 } = require("lodash");
-var _ = require('lodash');
+const _ = require('lodash');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+
+const homeStartingContent = "Dreams are mysterious things. Whether they're good or bad, tricks of memory or flights of fantasy, humans have been studying dreams and what they mean for a long time. Today, dreams play a role in the work of psychologists, scientists, artists, and even mathematicians! What role do dreams play in your life? You might be able to figure that out if you start keeping a dream journal.";
+
+const contactContent = "Having journaling ideas that you can use to write in your daily journal is important. Everyone gets writer's block from time to time, so having a list of ideas can help you to jump start your creativity.";
 
 const app = express();
 
@@ -36,10 +38,8 @@ app.get('/', function (req, res) {
   });
 });
 
-app.get('/about', function (req, res) {
-  res.render('about', {
-    aboutContent: aboutContent
-  })
+app.get('/why-write', function (req, res) {
+  res.render('why-write')
 })
 
 app.get('/contact', function (req, res) {
