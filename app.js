6aac8227051bfc570909c3c1f591fbb47bbde77f
karina ivanova <86211129+karina4840@users.noreply.github.com>
2022-02-15 12:38:24 +0800

    /delete post upd

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const {
-  render
+  render, redirect
 } = require("express/lib/response");
-const {
-  forEach,
-  lowerCase
-} = require("lodash");
-const _ = require('lodash');
 const mongoose = require("mongoose");
+const _ = require('lodash');
+const { toPlainObject } = require("lodash");
 
 const homeContent = "Dreams are mysterious things. Whether they're good or bad, tricks of memory or flights of fantasy, humans have been studying dreams and what they mean for a long time. Today, dreams play a role in the work of psychologists, scientists, artists, and even mathematicians! What role do dreams play in your life? You might be able to figure that out if you start keeping a dream journal.";
 
@@ -29,59 +26,63 @@ mongoose.connect("mongodb+srv://admin-karina:112545@cluster0.gbw5o.mongodb.net/j
 const postSchema = {
   title: String,
   content: String
- };
- 
+};
+
 const Post = mongoose.model("Post", postSchema);
 
 
 app.get('/', function (req, res) {
-  
-  Post.find({}, function(err, posts){
+
+  Post.find({}, function (err, posts) {
     res.render("home", {
       startingContent: homeContent,
       posts: posts
-      });
+    })
   });
 });
 
 app.get('/why-write', function (req, res) {
   res.render('why-write')
-})
+});
 
 app.get('/contact', function (req, res) {
   res.render('contact', {
     contactContent: contactContent
   })
-})
+});
 
 app.get('/compose', function (req, res) {
   res.render('compose')
-})
+});
 
 app.post('/compose', function (req, res) {
-  const post = new Post ({
+  const post = new Post({
     title: req.body.newTitle,
     content: req.body.newPost
   });
 
-  post.save(function(err){
-    if(!err){
-      res.redirect('/');    
+  post.save(function (err) {
+    if (!err) {
+      res.redirect('/');
     }
   })
-})
+});
 
-app.get('/posts/:postName', function (req, res) {
+app.get("/posts/:postId", (req, res) => {
 
-  const requestedPostName = req.params.postName;
-
-
-  Post.findOne({_id: requestedPostName}, function(err, post){
+  Post.findOne(
+    {_id: req.params.postId}, (err, post) => {
     res.render("post", {
       title: post.title,
       content: post.content
-    });
+    })
+    console.log(req.params.postId);
   });
+});
+
+app.post('/delete/:id', async (req, res) => {
+  await Post.deleteOne({_id: req.params.id})
+  return res.redirect('/')
 });
 
 
